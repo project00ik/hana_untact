af499ab378ed8f18df15b204204d682b052e67bf
inkyung <project00ik@gmail.com>
2023-05-26 17:53:35 +0900

    슬라이드 팝업 교체

--- hana_untact/js/common-ui.js
+++ hana_untact/js/common-ui.js
@@ -535,7 +535,7 @@ var hanaUI = {
         var nextInput = $('#' + 'inputMove' + nextNum);
         nextInput.closest('.form-area').addClass('active');
         nextInput.focus();
-        nextInput.trigger('click');
+        // nextInput.trigger('click');
     },
     
     
