7280a8d19773be999b4444f6ebb6e4e08b4e7a3d
inkyung <project00ik@gmail.com>
2023-06-15 15:02:01 +0900

    레이아웃 jquery로 수정

--- hana_untact/renew/bdm/js/common-ui.js
+++ hana_untact/renew/bdm/js/common-ui.js
@@ -39,21 +39,21 @@ var hanaUI = {
         Common UI
     *****************************/
     layout: function () { 
-        // var windowHeight = $(window).innerHeight();
-        // var headerHeight = $('.app-header').innerHeight();
-        // var footerHeight = $('.app-footer').outerHeight();
-        // var containerHeight = windowHeight - headerHeight - footerHeight;
-        // $('body').css('min-height', windowHeight + 'px');
-        // $('.app-content').css('min-height', containerHeight + 'px');
+        var windowHeight = $(window).innerHeight();
+        var headerHeight = $('.app-header').innerHeight();
+        var footerHeight = $('.app-footer').outerHeight();
+        var containerHeight = windowHeight - headerHeight - footerHeight;
+        $('body').css('min-height', windowHeight + 'px');
+        $('.app-content').css('min-height', containerHeight + 'px');
 
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty("--vh", `${vh}px`);
+        // let vh = window.innerHeight * 0.01;
+        // document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-        window.addEventListener("resize", () => {
-            console.log("resize");
-            let vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty("--vh", `${vh}px`);
-        });
+        // window.addEventListener("resize", () => {
+        //     console.log("resize");
+        //     let vh = window.innerHeight * 0.01;
+        //     document.documentElement.style.setProperty("--vh", `${vh}px`);
+        // });
     },
     header : function(obj){
         var $el = null;
