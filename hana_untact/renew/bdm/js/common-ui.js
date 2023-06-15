2003edf1b432974d45e6163f7263ab6053ec9bc3
inkyung <project00ik@gmail.com>
2023-06-15 14:15:51 +0900

    레이아웃 수정

--- hana_untact/renew/bdm/js/common-ui.js
+++ hana_untact/renew/bdm/js/common-ui.js
@@ -43,9 +43,15 @@ var hanaUI = {
         var headerHeight = $('.app-header').innerHeight();
         var footerHeight = $('.app-footer').outerHeight();
         var containerHeight = windowHeight - headerHeight - footerHeight;
-        console.log(headerHeight);
-        $('body').css('min-height', windowHeight + 'px');
+        // console.log(headerHeight);
+        // $('body').css('min-height', windowHeight + 'px');
         $('.app-content').css('min-height', containerHeight + 'px');
+        // let vh = window.innerHeight * 0.01
+        // document.documentElement.style.setProperty('--vh', `${vh}px`)
+        // window.addEventListener('resize', () => {
+        //     let vh = window.innerHeight * 0.01
+        //     document.documentElement.style.setProperty('--vh', `${vh}px`)
+        // })
     },
     header : function(obj){
         var $el = null;
