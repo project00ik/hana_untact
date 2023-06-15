5a165b638b6a6775f8ad68ff6ae9896902bff604
inkyung <project00ik@gmail.com>
2023-06-15 14:55:16 +0900

    body 스크롤 테스트

--- hana_untact/renew/bdm/js/common-ui.js
+++ hana_untact/renew/bdm/js/common-ui.js
@@ -50,9 +50,9 @@ var hanaUI = {
         document.documentElement.style.setProperty("--vh", `${vh}px`);
 
         window.addEventListener("resize", () => {
-        console.log("resize");
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty("--vh", `${vh}px`);
+            console.log("resize");
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty("--vh", `${vh}px`);
         });
     },
     header : function(obj){
